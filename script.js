311a09de41bbe1b92637e4a2d424d81233560f64
vivekjoshi818 <vj69376@gmail.com>
2024-01-14 02:51:48 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById('menu-btn');
+const nav = document.getElementById('menu')
+function navToggle () {
+    btn.classList.toggle('open')
+    nav.classList.toggle('hidden')
+}
+btn.addEventListener('click',navToggle)
